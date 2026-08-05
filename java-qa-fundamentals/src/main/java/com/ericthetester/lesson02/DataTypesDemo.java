43c0a10517c2bd25134b0862c799d8cfc88255c5
Emmanuel Agbiye <emmanuelagbiyerpn@gmail.com>
2026-08-05 17:16:36 +0100

    feat: complete lesson 02 data types and type casting

---
+++ java-qa-fundamentals/src/main/java/com/ericthetester/lesson02/DataTypesDemo.java
@@ -0,0 +1,39 @@
+package com.ericthetester.lesson02;
+
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        byte retryCount = 3;
+        short totalTests = 120;
+        int responseCode = 200;
+        long transactionId = 1234567890123L;
+        float cpuUsage = 72.5f;
+        double responseTime = 1.38;
+        char environment = 'Q';
+        boolean testPassed = true;
+
+        Integer age = 25;
+        Double salary = 45000.75;
+        Boolean automation = true;
+
+        System.out.println("Retry Count       : " + retryCount);
+        System.out.println("totalTests        : " + totalTests);
+        System.out.println("responseCode      : " + responseCode);
+        System.out.println("Transaction ID    : " + transactionId);
+        System.out.println("CPU Usage         : " + cpuUsage);
+        System.out.println("Response Time     : " + responseTime);
+        System.out.println("Environment       : " + environment);
+        System.out.println("Test Passed       : " + testPassed);
+        System.out.println("Integer Age       : " + age);
+        System.out.println("Double Salary     : " + salary);
+        System.out.println("Boolean Automation:"  + automation);
+
+
+        double score = 95.75;
+        int wholeScore = (int) score;
+        int number = 100;
+        double convertedNumber = number;
+        System.out.println("Original Score    : " + score);
+        System.out.println("Whole Score       :" + wholeScore);
+        System.out.println("Converted Number  : " + convertedNumber);
+    }
+}
